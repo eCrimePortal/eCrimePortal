d93c0d8a88f47f168fde6db289aacb9176e226e2
Desktop <akshitk40@gmail.com>
2020-08-25 16:34:56 +0530

    TOS, Refund Policy HREF, Route

--- routes/other.js
+++ routes/other.js
@@ -1,11 +1,14 @@
-'use strict';
+"use strict";
 
 require("dotenv").config();
 const express = require("express");
 var router = express.Router();
 const contributors = require("../models/contributors");
 router.get("/contributors", async (req, res) => {
-  res.render("other/contributors", {cbs: await contributors.find(), path: req.originalUrl});
+  res.render("other/contributors", {
+    cbs: await contributors.find(),
+    path: req.originalUrl,
+  });
 });
 router.get("/legal/terms", async (req, res) => {
   res.render("other/tos", { path: req.originalUrl });
@@ -13,13 +16,16 @@ router.get("/legal/terms", async (req, res) => {
 router.get("/legal/privacy", async (req, res) => {
   res.render("other/privacypolicy", { path: req.originalUrl });
 });
+router.get("/legal/refundpolicy", async (req, res) => {
+  res.render("other/refundPolicy", { path: req.originalUrl });
+});
 router.get("/legal/disclaimer", async (req, res) => {
-  res.render("other/disclaimer", {path: req.originalUrl});
+  res.render("other/disclaimer", { path: req.originalUrl });
 });
 router.get("/contribute", async (req, res) => {
-  res.render("other/contribute", {path: req.originalUrl});
+  res.render("other/contribute", { path: req.originalUrl });
 });
 router.post("/test", async (req, res) => {
-  res.send(req.body)
+  res.send(req.body);
 });
 module.exports = router;
